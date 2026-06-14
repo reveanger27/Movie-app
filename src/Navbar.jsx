354310e31dedf8039eae3e82240d17aa230351ff
Punda Setiawan <reveanger27@gmail.com>
2026-06-14 12:39:23 +0700

    fix navbar responsive

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,11 +2,11 @@ import {Link} from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav className="bg-white shadow-md px-8 py-2 flex gap-6 items-center">
-            <Link to="/" className="font-bold text-blue-600 text-xl mr-auto">🎬 MovieApp</Link>   
-            <Link to="/" className="text-gray-600 hover:text-blue-600 font-medium transition">Home</Link>
-            <Link to="/about" className="text-gray-600 hover:text-blue-600 font-medium transtion">About</Link>
-            <Link to="/movies" className="bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-blue-700 transition">Cari Film</Link>
+        <nav className="bg-white shadow-md px-4 py-2 flex gap-3 items-center">
+            <Link to="/" className="font-bold text-blue-600 text-lg mr-auto">🎬 MovieApp</Link>   
+            <Link to="/" className="text-gray-600 hover:text-blue-600 font-medium transition text-sm">Home</Link>
+            <Link to="/about" className="text-gray-600 hover:text-blue-600 font-medium transition text-sm">About</Link>
+            <Link to="/movies" className="bg-blue-600 text-white px-3 py-1.5 rounded-full hover:bg-blue-700 transition text-sm">Cari Film</Link>
         </nav>
     );
 }
